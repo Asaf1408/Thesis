c1f1ac8de834562932f998925252097f07fd3031
Asaf <asaf1408@hotmail.com>
2020-11-17 21:37:52 +0200

    optimization 4

---
+++ Semester A/Optimization 1/HW4/Gradiant_Backtracking.m
@@ -0,0 +1,33 @@
+function [x_k,iterations,converged] = Gradiant_Backtracking(func,grad_func,s,alpha,beta,x0)
+    
+    converged = 1;
+    
+    % maximum allowed number of iterations
+    max_iterations = 1000000;
+    
+    % set initial point
+    x_k = x0;
+    
+    % run until stopping criteria is satisfied
+    iterations = 0;
+    while ((norm(grad_func(x_k))>1e-5) && (iterations < max_iterations))
+        iterations = iterations + 1;
+        %disp("iteration: "+num2str(iterations));
+        % set initial backtracking step size    
+        t_k = s;
+        
+        % find sufficient backtracking step size
+        while (func(x_k)-func(x_k-t_k*grad_func(x_k)) < alpha * t_k * norm(grad_func(x_k))^2)
+            t_k = beta * t_k;
+        end
+        
+        % update solution
+        x_k = x_k - t_k * grad_func(x_k);
+    end
+    
+    % check if convergence achived
+    if (iterations == max_iterations)
+        converged = -1;
+    end    
+end
+
---
+++ Semester A/Optimization 1/HW4/Gradiant_Newton_Backtracking.m
@@ -0,0 +1,46 @@
+function [x_k,iterations,converged] = Gradiant_Newton_Backtracking(func,grad_func,hessian_func,s,alpha,beta,x0)
+    
+    converged = 1;
+    
+    % maximum allowed number of iterations
+    max_iterations = 1000000;
+
+    % set initial point
+    x_k = x0;
+    
+    % run until stopping criteria is satisfied
+    iterations = 0;
+    while ((norm(grad_func(x_k))>1e-5) && (iterations < max_iterations))
+        iterations = iterations + 1;
+        %disp("iteration: "+num2str(iterations));
+        % set initial backtracking step size    
+        t_k = s;
+        
+        % check if the hessian is positive definith using cholesky
+        % composition
+        [L,p] = chol(hessian_func(x_k),'lower');
+        
+        % if it does use the newton direction
+        if (p==0)
+            d_k = L'\(L\grad_func(x_k));
+            
+        % otherwise, use the gradient direction
+        else
+            d_k = grad_func(x_k);
+        end   
+        
+        % find sufficient backtracking step size
+        while (func(x_k)-func(x_k-t_k*d_k) < alpha * t_k * grad_func(x_k)'*d_k)
+            t_k = beta * t_k;
+        end
+        
+        % update solution
+        x_k = x_k - t_k * d_k;
+    end
+    
+    % check if convergence achived
+    if (iterations == max_iterations)
+        converged = -1;
+    end    
+end
+
